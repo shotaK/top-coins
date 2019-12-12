580a2a55e9eab05310e692d55ff68e82ab8e6fbd
Shota Karkashadze <sh.karkashadze@gmail.com>
2019-12-12 22:42:28 +0400

    Updates

---
+++ precache-manifest.64891983ea1a3f1d727d7a9186b95bc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a63eba43fb8fac8543b1843285d8db1",
+    "url": "/challenge/index.html"
+  },
+  {
+    "revision": "6a80d6de64e2942d6545",
+    "url": "/challenge/static/js/2.358c1c0b.chunk.js"
+  },
+  {
+    "revision": "2a4d3813bdbfda79df1f9834c55ea045",
+    "url": "/challenge/static/js/2.358c1c0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1833b60f055bd35dc616",
+    "url": "/challenge/static/js/main.84a06158.chunk.js"
+  },
+  {
+    "revision": "d465d82c292e5d21a9d9",
+    "url": "/challenge/static/js/runtime-main.c1e6b964.js"
+  }
+]);
